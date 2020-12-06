d9ad4b2f2c8bbe6cd42612428dbc858765a4c784
Genérico - Suporte <gsuporte@proway.treina>
2020-12-06 15:06:01 -0300

    Java_Fundamentos_Aula01

---
+++ Java_Fundamentos/src/java_fundamentos/Java_Fundamentos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_fundamentos;
+
+/**
+ *
+ * @author gsuporte
+ */
+public class Java_Fundamentos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá Mundo!");
+    }
+    
+}
