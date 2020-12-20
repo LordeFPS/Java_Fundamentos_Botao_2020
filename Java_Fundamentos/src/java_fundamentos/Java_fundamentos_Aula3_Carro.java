3ec3bd248126f1b4e6e1f6a6cc99276ae108b4de
lordefps <lodizinho_mice@hotmail.com>
2020-12-20 14:32:25 -0300

    Aula3_Cadastro_Carros

---
+++ Java_Fundamentos/src/java_fundamentos/Java_fundamentos_Aula3_Carro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_fundamentos;
+
+/**
+ *
+ * @author 69146
+ */
+public class Java_fundamentos_Aula3_Carro {
+    
+    String marca;
+    String modelo;
+    String placa;
+    String combutivel;
+    int    nportas;
+    int    capacidade;
+    int    cv;
+}
