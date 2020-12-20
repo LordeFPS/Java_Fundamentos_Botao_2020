4c408101816e1e0adc2aa4ea35719975d3174047
lordefps <lodizinho_mice@hotmail.com>
2020-12-20 14:32:51 -0300

    Aula3_Instanciando_obejto

---
+++ Java_Fundamentos/src/java_fundamentos/Java_Fundamentos_Aula3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_fundamentos;
+
+/**
+ *
+ * @author 69146
+ */
+public class Java_Fundamentos_Aula3 {
+  
+    public static void main(String[] args) {
+       // instanciar um objeto
+       
+       
+       Java_fundamentos_Aula3_Carro obj = new Java_fundamentos_Aula3_Carro();
+       obj.marca  = "Ford";
+       obj.modelo = "Ka";
+       obj.placa  = "abc-1234";
+       obj.combutivel = "flex";
+       obj.nportas    = 2;
+       obj.capacidade = 4;
+       obj.cv = 78;
+    }
+    
+}
+
