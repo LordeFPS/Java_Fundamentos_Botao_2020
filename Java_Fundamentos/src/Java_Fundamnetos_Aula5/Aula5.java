9dd63c430966674d2a12549582f007031cde61a0
lordefps <lodizinho_mice@hotmail.com>
2021-01-31 14:10:43 -0300

    Aula 5

---
+++ Java_Fundamentos/src/Java_Fundamnetos_Aula5/Aula5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Fundamnetos_Aula5;
+
+import Java_Fundamnetos_Aula5.Entidade.Venda;
+
+/**
+ *
+ * @author 69146
+ */
+public class Aula5 {
+    
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        p.nome = "GABRIEL";
+        p.setCpf("1234");
+        
+        Venda v = new Venda();
+        
+    }
+    
+    
+}
