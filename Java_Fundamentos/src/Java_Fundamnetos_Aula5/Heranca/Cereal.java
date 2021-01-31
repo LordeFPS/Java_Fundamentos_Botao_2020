b7dd5fbad0f2286b6cc806c6e8efd71e13c26b55
lordefps <lodizinho_mice@hotmail.com>
2021-01-31 16:46:02 -0300

    Criando Classe Cereal

---
+++ Java_Fundamentos/src/Java_Fundamnetos_Aula5/Heranca/Cereal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Fundamnetos_Aula5.Heranca;
+
+/**
+ *
+ * @author 69146
+ */
+public class Cereal extends Produto{
+    private String tipoEmbalagem;
+
+    public Cereal(){
+        
+    }
+    
+    public Cereal(Cereal cereal) {
+        this.tipoEmbalagem = cereal.getTipoEmbalagem();
+    }
+    
+    public String getTipoEmbalagem() {
+        return tipoEmbalagem;
+    }
+
+    public void setTipoEmbalagem(String tipoEmbalagem) {
+        this.tipoEmbalagem = tipoEmbalagem;
+    }
+    
+    public void leitor(){
+        System.out.println("Tipo de Embalagem: " + getTipoEmbalagem());
+    }
+    
+}
