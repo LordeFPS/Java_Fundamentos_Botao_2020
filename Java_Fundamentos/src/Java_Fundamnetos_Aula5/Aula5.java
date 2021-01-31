78ba444891ddd7ccf2036e44bb4feb797d61b505
lordefps <lodizinho_mice@hotmail.com>
2021-01-31 14:26:05 -0300

    Configurando Main

--- Java_Fundamentos/src/Java_Fundamnetos_Aula5/Aula5.java
+++ Java_Fundamentos/src/Java_Fundamnetos_Aula5/Aula5.java
@@ -14,13 +14,27 @@ import Java_Fundamnetos_Aula5.Entidade.Venda;
 public class Aula5 {
     
     public static void main(String[] args) {
+        
+        //Classes();
+        ExFinal();
+        
+    }
+    
+    public static void ExFinal() {
+        ExemploFinal exfinal = new ExemploFinal();
+        System.out.println("Valor de x " + exfinal.x);
+        //exfinal.x = 50;
+        exfinal.y = 100;
+        System.out.println("Resultado " + exfinal.Soma());
+    }
+    
+    public static void Classes() {
         Pessoa p = new Pessoa();
         p.nome = "GABRIEL";
         p.setCpf("1234");
         
         Venda v = new Venda();
-        
+
     }
-    
     
 }
---
+++ Java_Fundamentos/src/Java_Fundamnetos_Aula5/ExemploFinal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Fundamnetos_Aula5;
+
+/**
+ *
+ * @author 69146
+ */
+public class ExemploFinal {
+    //final faz com que você não possa manipular esta variável em outro lugar
+    final int x = 10; 
+    int y = 20;
+    
+    public int Soma(){
+        
+        return x + y;
+    }
+    
+}
