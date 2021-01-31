831a7bcfab2c0dcc63d7dca1b719ca6510d87ca8
lordefps <lodizinho_mice@hotmail.com>
2021-01-31 15:31:24 -0300

    Criando classe filha com extends

---
+++ Java_Fundamentos/src/Java_Fundamnetos_Aula5/Heranca/Motocicleta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Fundamnetos_Aula5.Heranca;
+
+/**
+ *
+ * @author 69146
+ */
+public class Motocicleta extends Veiculo{
+    //usando o "extends" a calsse Motocicleta passa a ter os atributos
+    //da classe veiculo, logo a classe Veiculo é "pai" da classe Motocicleta
+    private int cilindrada;
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+    
+    
+}
