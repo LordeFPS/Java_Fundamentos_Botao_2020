602869a5bb81a478d7d722bcb22e05fdc04a5a9f
lordefps <lodizinho_mice@hotmail.com>
2021-01-31 14:11:01 -0300

    Entidade Venda

---
+++ Java_Fundamentos/src/Java_Fundamnetos_Aula5/Entidade/Venda.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Fundamnetos_Aula5.Entidade;
+
+/**
+ *
+ * @author 69146
+ */
+public class Venda {
+    
+}
